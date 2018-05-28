45e277003bb4d6065e4c08423d168a0b5175ef8d
fatchan <admin@fatpeople.lol>
2018-05-28 19:30:51 +1000

    all done :ok_hand:

--- flink/src/MostPopularAircraft.java
+++ flink/src/MostPopularAircraft.java
@@ -1,6 +1,14 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatJoinFunction;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
+import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -23,8 +31,6 @@ public class MostPopularAircraft {
 		DataSet<Tuple3<String, String, String>> airlines = env
 				.readCsvFile("hdfs://127.0.0.1:9000/user/hadoop/ontimeperformance_airlines.csv")
 				.includeFields("111").ignoreFirstLine().ignoreInvalidLines().types(String.class, String.class, String.class);
-	
-		
 		// Define a data set from the FLIGHTS file , include the required fields for
 		// the task, in this case airport_code
 		DataSet<Tuple2<String, String>> flights = env
@@ -36,33 +42,51 @@ public class MostPopularAircraft {
 		DataSet<Tuple3<String, String, String>> aircraft = env
 				.readCsvFile("hdfs://127.0.0.1:9000/user/hadoop/ontimeperformance_aircrafts.csv").includeFields("101010000")
 				.ignoreFirstLine().ignoreInvalidLines().types(String.class, String.class, String.class);
-
 		DataSet<Tuple3<String, String, String>> USairlines = airlines.filter(new StringFilter());
 		DataSet<Tuple2<String, String>> Tailcodes = flights.filter(new tailcodeFilter());
-		
-		//USairlines.first(10).print(); // show filter worked
-		
 		// Join to data sets to create tuple data set with tail_numbers, airline name
 		DataSet<Tuple2<String,String>> resultJoin1 = USairlines.join(Tailcodes).where(0).equalTo(0)// joining two data sets
 																			.with(new JoinAlF()); // using a new join function to create the tuple
-		System.out.println("First Join:");
-		//resultJoin1.first(10).print(); //show join worked
-		
 		// need tuple 3 for airline name, manufacturer and model
 		DataSet<Tuple3<String, String, String>> resultJoin2 = resultJoin1.join(aircraft).where(1).equalTo(0).with(new JoinAR());
-																				
-		// Output should be Airline_name, Manufacturer, Model
-		System.out.println("!!! Second Join:");
-		//resultJoin2.groupBy(0, 1, 2).first(10).print();
 		
-		DataSet<Tuple3<String,String,Integer>> resultCount = resultJoin2
+		//airline, manufacturer+' '+model, count
+		List<Tuple3<String,String,Integer>> results = resultJoin2
 				.groupBy(0, 1, 2) 
 				.reduceGroup(new PCounter())
 				.sortPartition(0, Order.DESCENDING)
-				.sortPartition(2, Order.DESCENDING);
-		System.out.println("Count Result:");
-		resultCount.first(50).print();
+				.sortPartition(2, Order.DESCENDING)
+				.collect();
 
+		try {
+            Writer output = null;
+            String path = "/home/hadoop/MostPopular_results.txt";
+            File file = new File(path);
+            output = new BufferedWriter(new FileWriter(file));
+            String airline = null;
+            ArrayList<String> addedairlines = new ArrayList<String>();  
+            String[] line = new String[5];
+            int num = 0;
+            for (Tuple3<String, String, Integer> row : results) {
+            	if (addedairlines.contains(row.f0)) {continue;} else {
+                	airline = row.f0;
+            	}
+            	if (num >= 5 || !row.f0.equals(airline)) {
+            		System.out.println(num+" "+airline+"\t["+String.join(",", line)+"]");
+            		output.write(airline+"\t["+String.join(",", line)+"]\n");
+            		addedairlines.add(airline);
+            		line = new String[5];
+            		num = 0;
+            	}
+            	line[num] = row.f1;
+            	num++;
+            }
+            output.close();
+            System.out.println("Wrote to file "+path);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+		
 	}
 	
 	// need to match on the f2 column of the second join ouptput and count.
@@ -81,11 +105,11 @@ public class MostPopularAircraft {
 		}
 	}
 	
-	public static class JoinAR implements FlatJoinFunction<Tuple2<String, String>, Tuple3<String,String,String>, Tuple3<String,String,String>> {
-		public void join(Tuple2<String,String> result1, Tuple3<String,String,String> aircrafts, Collector<Tuple3<String,String,String>> out) throws Exception {
-			out.collect(new Tuple3<String,String,String>(result1.f0, aircrafts.f1, aircrafts.f2));
-		}
-	}
+    public static class JoinAR implements JoinFunction<Tuple2<String, String>, Tuple3<String,String,String>, Tuple3<String,String,String>> {
+        public Tuple3<String, String, String> join(Tuple2<String,String> result1, Tuple3<String,String,String> airlines) throws Exception {
+                return new Tuple3<>(result1.f0, airlines.f1, airlines.f2);
+        }
+    }
 	public static class JoinAlF implements FlatJoinFunction<Tuple3<String, String, String>, Tuple2<String,String>, Tuple2<String,String>> {
 		public void join(Tuple3<String, String,String> airlines, Tuple2<String,String> flights, Collector<Tuple2<String,String>> out) throws Exception {
 			if (!flights.f1.equals("")) {
